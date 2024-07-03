36908b6567f1a0edcc902b5ac2289074dd827f15
David Roig Salgado <david.roig.salgado@gmail.com>
2024-07-03 10:58:14 -0400

    better proxy and new default options

--- index.js
+++ index.js
@@ -7,133 +7,142 @@ const proxy = require('http-proxy-middleware');
 const program = require('commander');
 require('colors');
 const { version } = require('./package.json');
+const zlib = require('zlib');
+
 const cacheDir = 'mocks';
 const confFileName = 'proxy-mock.conf.json';
 const confLocation = process.cwd() + '/' + confFileName;
 const dirCacheFile = process.cwd() + '/' + cacheDir;
-const zlib = require('zlib');
-
 
 program
   .version(version)
   .option('-p, --port [port number]', 'port to use (default is 3000)')
-	.parse(process.argv);
+  .parse(process.argv);
 
-const PORT = parseInt(program.port) || 3000;
+const PORT = parseInt(program.port) || getLocalConfig()?.port || 3000;
 
 
 const baseConfig = {
-	host: "http://localhost:8080",
-	routeConfig: [
-		{
-			expression: "delayed/endpoint",
-			delay: 5,
-			status: 500,
-			body: "This is a custom response body!"
-		}
-	]
+  host: "http://localhost:8080",
+  excludeUrls: ["path/to/exclude"],
+  port: PORT,
+  routeConfig: [
+    {
+      expression: "delayed/endpoint",
+      delay: 5,
+      status: 500,
+      body: "This is a custom response body!"
+    }
+  ]
 }
 
 
 function getLocalConfig() {
-    let localConfig;
-    try {
-			//localConfig = require(confLocation);
-			let rawdata = fs.readFileSync(confLocation);  
-			localConfig = JSON.parse(rawdata); 
-    } catch(e) {
-			localConfig = baseConfig;
+  let localConfig;
+  try {
+    //localConfig = require(confLocation);
+    let rawdata = fs.readFileSync(confLocation);
+    localConfig = JSON.parse(rawdata);
+  } catch (e) {
+    localConfig = baseConfig;
 
-			let jsonConfig = JSON.stringify(localConfig, null, '\t');
-      fs.writeFileSync(confLocation, jsonConfig, 'utf8', function (err) {
-        if (err) {
-					return console.log(`Couldn't create initial config file.`);
-        }
-      });
-    }
-    return localConfig;
+    let jsonConfig = JSON.stringify(localConfig, null, '\t');
+    fs.writeFileSync(confLocation, jsonConfig, 'utf8', function (err) {
+      if (err) {
+        return console.log(`Couldn't create initial config file.`);
+      }
+    });
+  }
+  return localConfig;
 }
 
 let config = getLocalConfig();
 
 
 function getFileName(url, method) {
-	return url
-			.replace(/^\//, "").replace(/\/$/, "")
-			.replace(/\//g, ".")
-			//.replace(/\/?\.[0-9]+-[0-9kK]+/, "")     // quito rut
-			+ `.${method}.json`;
+  return url
+    .replace(/^\//, "").replace(/\/$/, "")
+    .replace(/\//g, ".")
+    //.replace(/\/?\.[0-9]+-[0-9kK]+/, "")     // quito rut
+    + `.${method}.json`;
 }
 
 
 function useCacheMiddleware(req, res, next) {
 
-	if (config.skipCache) return next();
-	
-	let cacheFile = dirCacheFile + '/' + getFileName(req.url, req.method);
+  if (config.skipCache) return next();
 
-	if (!fs.existsSync(cacheFile)) return next();
+  let cacheFile = dirCacheFile + '/' + getFileName(req.url, req.method);
 
-	let output = fs.readFileSync(cacheFile, {encoding: 'utf8'});
+  if (!fs.existsSync(cacheFile)) return next();
 
-	if (fs.existsSync(cacheFile)) {
-		console.log('Usando cache para', req.url.cyan);
-		res.end(output);
-	} else {
-		next();
-	}
+  let output = fs.readFileSync(cacheFile, { encoding: 'utf8' });
+
+  if (!isInExcludeUrls(req) && fs.existsSync(cacheFile)) {
+    console.log('Usando cache para', req.url.cyan);
+    res.end(output);
+  } else {
+    next();
+  }
+}
+
+function isInExcludeUrls(req) {
+  const excluding = config.excludeUrls && config.excludeUrls.some((regex) => (new RegExp(regex)).test(req.url));
+  if (excluding) {
+    console.log('Excluding URL: ', req.url.cyan);
+  }
+  return excluding
 }
 
 
-
 function makeProxy() {
-	return proxy({
+  return proxy({
     target: config.host,
-		//secure: false,
-		changeOrigin: true,
-    onProxyReq: function(proxyReq, req, res) {
-			//proxyReq.setHeader('foo', 'bar' );
+    //secure: false,
+    changeOrigin: true,
+    onProxyReq: function (proxyReq, req, res) {
+      //proxyReq.setHeader('foo', 'bar' );
     },
 
-    onProxyRes: function(proxyRes, req, res) {
+    onProxyRes: function (proxyRes, req, res) {
       const _write = res.write;
       const _end = res.end;
       let output;
-			let body = "";
-			let resArr = [];
-			
-			const isGzipped = proxyRes.headers["content-encoding"] == "gzip";
-		
-      proxyRes.on('data', function(data) {
+      let body = "";
+      let resArr = [];
+
+      const isGzipped = proxyRes.headers["content-encoding"] == "gzip";
+
+      proxyRes.on('data', function (data) {
         //data = data.toString('utf-8');
-				body += data;
-				resArr.push(data);
+        body += data;
+        resArr.push(data);
       });
 
       // Defer all writes
-      res.write = () => {};
-      res.end = function() {
-				output = body;
-				const buffer = Buffer.concat(resArr);
+      res.write = () => { };
+      res.end = function () {
+        output = body;
+        const buffer = Buffer.concat(resArr);
 
-				let cacheFile = dirCacheFile + '/' + getFileName(req.url, req.method);
+        let cacheFile = dirCacheFile + '/' + getFileName(req.url, req.method);
 
-				if (isGzipped) {
-					zlib.gunzip(buffer, function (err, dezipped) {
-						if (err) throw err;
-						let json = dezipped.toString();
-						res.setHeader("content-encoding", null);
-						_end.apply( res, [json] );
-						saveResponse(json, req, res, dirCacheFile, cacheFile);
-						res.write = _write;
-					});
-				} else {
-					let json = output.toString('utf-8');
-					_end.apply( res, [json] );
-					saveResponse(json, req, res, dirCacheFile, cacheFile);
-					res.write = _write;
-				}
-				
+        if (isGzipped) {
+          zlib.gunzip(buffer, function (err, dezipped) {
+            if (err) throw err;
+            let json = dezipped.toString();
+            res.setHeader("content-encoding", null);
+            _end.apply(res, [json]);
+            saveResponse(json, req, res, dirCacheFile, cacheFile);
+            res.write = _write;
+          });
+        } else {
+          let json = output.toString('utf-8');
+          _end.apply(res, [json]);
+          saveResponse(json, req, res, dirCacheFile, cacheFile);
+          res.write = _write;
+        }
+
       }
     },
 
@@ -145,83 +154,83 @@ function makeProxy() {
 
 /**
 * Save response to JSON file
-* @param {string} body 
-* @param {Object} res 
+* @param {string} body
+* @param {Object} res
 */
 function saveResponse(body, req, res, cacheDir, cacheFile) {
-	let contentType = res.getHeader('Content-Type');
+  let contentType = res.getHeader('Content-Type');
 
-	//if (/json/i.test(contentType) && res.statusCode === 200) {
-	if (/json/i.test(contentType) && [200,201,204].includes(res.statusCode)) {
-		// crear directorio si no existe
-		if (!fs.existsSync(cacheDir)){
-				fs.mkdirSync(cacheDir);
-		}
+  //if (/json/i.test(contentType) && res.statusCode === 200) {
+  if (/json/i.test(contentType) && [200, 201, 204].includes(res.statusCode)) {
+    // crear directorio si no existe
+    if (!fs.existsSync(cacheDir)) {
+      fs.mkdirSync(cacheDir);
+    }
 
-		fs.writeFile(cacheFile, body, 'utf8', function (err) {
-			if (err) {
-				return console.log(err);
-			}
-		});
-	}
+    fs.writeFile(cacheFile, body, 'utf8', function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
 }
 
 
 /**
- * 
+ *
  */
 function customResponseMiddleware(req, res, next) {
-	if (req.method === 'OPTIONS') return next();
-	for (let route of config.routeConfig) {
-		if ((new RegExp(route.expression)).test(req.url)) {
-			if (route.status && [200,204].indexOf(route.status)<0) {
-				res.status(route.status).send(route.body);
-			}
-		}
-	}
+  if (req.method === 'OPTIONS') return next();
+  for (let route of config.routeConfig) {
+    if ((new RegExp(route.expression)).test(req.url)) {
+      if (route.status && [200, 204].indexOf(route.status) < 0) {
+        res.status(route.status).send(route.body);
+      }
+    }
+  }
 
-	next();
+  next();
 }
 
 
 function delaysMiddleware(req, res, next) {
-	if (req.method === 'OPTIONS') return next();
-	let delay = 0;
-	for (let route of config.routeConfig) {
-		if ((new RegExp(route.expression)).test(req.url)) {
-			delay = route.delay || 0;
-		}
-	}
-	
-	setTimeout(() => {
-		next();
-	}, delay*1000);
+  if (req.method === 'OPTIONS') return next();
+  let delay = 0;
+  for (let route of config.routeConfig) {
+    if ((new RegExp(route.expression)).test(req.url)) {
+      delay = route.delay || 0;
+    }
+  }
+
+  setTimeout(() => {
+    next();
+  }, delay * 1000);
 }
 
 function initServer(restarted) {
-	app.use(function(req, res, next) {
-		res.setHeader('Access-Control-Allow-Origin', '*');
-		res.setHeader('Access-Control-Allow-Methods', '*');
-		res.setHeader('Access-Control-Allow-Headers', '*');
-	
-		next();
-	})
-	.use(delaysMiddleware)
-	.use(customResponseMiddleware)
-	.use(useCacheMiddleware)
-	.use(makeProxy());
-	
-	const server = app.listen(PORT, function () {
-		console.log( restarted ? 'Server restarted' : `Backend proxy running on port ${PORT}`);
-	});
-	return server;
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    next();
+  })
+    .use(delaysMiddleware)
+    .use(customResponseMiddleware)
+    .use(useCacheMiddleware)
+    .use(makeProxy());
+
+  const server = app.listen(PORT, function () {
+    console.log(restarted ? 'Server restarted' : `Backend proxy running on port ${PORT}`);
+  });
+  return server;
 }
 
 let server = initServer();
 
 fs.watchFile(confLocation, { interval: 1000 }, () => {
-	config = getLocalConfig();
-	server.close(() => {
-		server = initServer(true);
-	})
+  config = getLocalConfig();
+  server.close(() => {
+    server = initServer(true);
+  })
 });
